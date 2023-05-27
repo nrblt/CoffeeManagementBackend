9eab77c13236fe3a69ebacd6279a1da190de913d
Nurbolat <nurbolat@gmail.com>
2023-05-27 17:45:44 +0600

    todays income

--- admin_app/views.py
+++ admin_app/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, ViewSet
-
+import datetime
 from staff.models import StaffAccount
-
+from order.models import Order
 from .serializers import PositionSerializer
 
 User = get_user_model()
@@ -25,4 +25,13 @@ class AdminViewSet(ViewSet):
             return Response({'position': is_staff.position})
         else:
             return Response({'position': 'customer'})
-        
+    def todaysIncome(self, request):
+        today_min = datetime.datetime.combine(datetime.date.today(), datetime.time.min)
+        today_max = datetime.datetime.combine(datetime.date.today(), datetime.time.max)
+        orders = Order.objects.filter(created_at__range=(today_min, today_max))
+        today_earning = int(0)
+        for order in orders:
+            today_earning += order.total_price
+        context = {}
+        context['today_earning'] = today_earning
+        return Response(context)
